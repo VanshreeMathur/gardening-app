37cba122519290a6ef849434301549d60965fdc4
Vanshree Mathur <vanshreemathur@gmail.com>
2020-11-25 23:14:26 -0500

    Login page -> our UI design; still missing logo and black container for login details

--- App.js
+++ App.js
@@ -1,21 +1,139 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+//
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+//
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}> Nourish Project </Text>
+        <Text style={styles.logo2}> For Peterborough Gardeners </Text>
+
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email"
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+
+        <View style={styles.inputView} >
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.signupBtn}>
+          <Text style={styles.loginText}>Create a New Account</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#29C16C',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    // fontWeight:"bold",
+    fontSize:50,
+    color:"#ffffff",
+    marginBottom:15
+  },
+  logo2:{
+    // fontWeight:"bold",
+    fontSize:20,
+    color:"#ffffff",
+    marginBottom:40
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#ffffff",
+    borderRadius:15,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  // blackBox:{
+  //   width:"80%",
+  //   backgroundColor:"#000000",
+  //   borderRadius:15,
+  //   height:50,
+  //   marginBottom:20,
+  //   justifyContent:"center",
+  //   padding:20
+  // },
+  inputText:{
+    height:50,
+    color:"#003f5c"
+  },
+  forgot:{
+    color:"white",
+    fontSize:13,
+    textDecorationLine: 'underline'
+  },
+  loginBtn:{
+    width:"50%",
+    backgroundColor:"#EE7729",
+    borderRadius:15,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white",
+  },
+  signupBtn:{
+    width:"100%",
+    backgroundColor:"#EE7729",
+    borderRadius:0,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    position: "absolute",
+    bottom:0,
+    marginTop:40,
+    // marginBottom:10
+  }
 });
