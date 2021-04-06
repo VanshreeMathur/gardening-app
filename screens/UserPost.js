5d758d09f80be48cda01b017df67f749c83b9537
Matthew Allen <matthew.allen@bell.net>
2021-04-06 19:26:43 -0400

    Updated UserPost (No Date Pickers Yet)

--- screens/UserPost.js
+++ screens/UserPost.js
@@ -9,13 +9,20 @@ import { Amplify, API, graphqlOperation } from 'aws-amplify'
 import { createUserPost } from '../graphql/mutations'
 import { listUserPosts } from '../graphql/queries'
 
+import RadioButtonRN from 'radio-buttons-react-native';
+import RNPickerSelect from 'react-native-picker-select';
+
 import awsconfig from '../aws-exports'
+import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
+
 Amplify.configure(awsconfig)
 
-const initialState = {user_id: 1, product_type: '', product_size: 0, product_quantity: 0, timeline_start: "", timeline_end: ""};
+const initialState = {user_id: 1, product_type: -1, product_size: 0, product_quantity: 0, timeline_start: "", timeline_end: ""};
 
 export default function UserPost(){ 
   
+  // this.state = {date: Date.now()}
+
   const [formState, setFormState] = useState(initialState);
   const [userPosts, setUserPosts] = useState([]);
 
@@ -35,7 +42,7 @@ export default function UserPost(){
       const userPosts = userPostData.data.listUserPosts.items;
       setUserPosts(userPosts);
 
-    } catch (err) { console.log('Error fetching posts.')};
+    } catch (err) { {/*console.log('Error fetching posts.') */}};
 
   }
 
@@ -48,55 +55,75 @@ export default function UserPost(){
       await API.graphql(graphqlOperation(createUserPost, {input: userPost}));
       
     } catch (err) {
-      console.log('Error creating post:', err);
+      // console.log('Error creating post:', err);
     }
   }
+
+  const buttonData = [
+    {
+      label: 'Large',
+      value: 0
+    },
+    {
+      label: 'Medium',
+      value: 1
+    },
+    {
+      label: 'Small',
+      value: 2
+    }
+  ]
+
+  
   
     return (
-        <View style={styles.container}>
-            
-            <Text style={styles.logo}> My Harvest </Text>
-            
-            <View style={styles.inputView} >
-            <TextInput
-                onChangeText={val => setInput('product_type', val)}
-                style={styles.inputText}
-                value={formState.product_type}
-                placeholder="Product Type"
-                />
-            </View>
-
-            <View style={styles.btnBox}>
-            
-            <Button 
-                style={styles.newBtn}
-                title="BIG"
-                onPress={ () => { setInput('product_size', 0)}}
+      <View style={styles.container}>
+        <Text style={styles.logo}> My Harvest </Text>
+        
+          <View style={styles.picker}>
+            <RNPickerSelect
+              onValueChange={(value) => setInput('product_type',value)}
+              items={[
+                { label: 'Tomatoes', value: 0},
+                { label: 'Lettuce', value: 1},
+                { label: 'Kale', value: 2},
+                { label: 'Carrots', value: 3},
+                { label: 'Peppers', value: 4},
+                { label: 'Radishes', value: 5},
+                { label: 'Potatoes', value: 6},
+                { label: 'Squash', value: 7},
+                { label: 'Cucumbers', value: 8},
+                { label: 'Beans', value: 9},
+                { label: 'Garlic', value: 10},
+                { label: 'Beets', value: 11}
+              ]}
+              style={customPickerStyles.inputIOS}
             />
-            
-            <Button 
-                style={styles.newBtn}
-                title="MED"
-                onPress={ () => { setInput('product_size', 1)}}
-            />
+          </View>
 
-            <Button 
-                style={styles.newBtn}
-                title="SML"
-                onPress={ () => { setInput('product_size', 3)}}
-            />
-            </View>
+        <View style={styles.btnBox}>
 
-            <View style={styles.inputView} >
-            <TextInput
-                onChangeText={val => setInput('product_quantity', val)}
-                style={styles.inputText}
-                value = {formState.product_quantity}
-                placeholder = "Product Quantity"
-                />
-            </View>
-            
-            <View style={styles.inputView} >
+          <RadioButtonRN
+            data = {buttonData}
+            selectedBtn = {(e) => setInput('product_size', e.value)}
+            style = {styles.newBtn}
+          />
+
+        </View>
+
+        <View style={styles.inputView} >
+        <TextInput
+            onChangeText={val => setInput('product_quantity', val)}
+            style={styles.inputText}
+            value = {formState.product_quantity}
+            placeholder = "Product Quantity"
+            />
+        </View>
+          
+
+        {/* DATE PICKERS HERE */}
+
+        <View style={styles.inputView} >
             <TextInput
                 onChangeText={val => setInput('timeline_start', val)}
                 style={styles.inputText}
@@ -114,27 +141,84 @@ export default function UserPost(){
                 />
             </View>
 
-            <Button title="Create Post" onPress={addUserPost} />
-            {
+        <TouchableOpacity title="Create Post" style= {styles.submit} onPress={addUserPost} >
+          <Text style = {styles.btnText}> Create Post </Text>
+        </TouchableOpacity>
+        
+        {
 
-            userPosts.map((userPost, index) => (
-                
-                <View key={userPost.id ? userPost.id: index}>
-                <Text>{userPost.user_id}</Text>
-                <Text>{userPost.product_type}</Text>
-                <Text>{userPost.product_size}</Text>
-                <Text>{userPost.product_quantity}</Text>
-                <Text>{userPost.timeline_start}</Text>
-                <Text>{userPost.timeline_end}</Text>
-                </View>
+        userPosts.map((userPost, index) => (
+            
+            <View key={userPost.id ? userPost.id: index}>
+            <Text>{userPost.user_id}</Text>
+            <Text>{userPost.product_type}</Text>
+            <Text>{userPost.product_size}</Text>
+            <Text>{userPost.product_quantity}</Text>
+            <Text>{userPost.timeline_start}</Text>
+            <Text>{userPost.timeline_end}</Text>
+            </View>
 
-            ))
-            }
-        </View>
+        ))
+        }
+      </View>
     )    
 }
 
+
+
+const customPickerStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 14,
+    paddingTop: 13,
+    paddingHorizontal: 10,
+    height: 30,
+    color: 'black',
+    paddingBottom: 12, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 14,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 1,
+    borderColor: 'blue',
+    borderRadius: 8,
+    color: 'black',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+});
+
 const styles = StyleSheet.create({
+  picker: {
+    width:"80%",
+    backgroundColor:"#ffffff",
+    borderRadius:15,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20,
+  },
+  btnText: {
+    color: 'white'
+  },
+  newBtn: {
+    width: 200,
+    height: 50,
+    color: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+  submit: {
+    backgroundColor: '#444',
+    padding: 15,
+    borderRadius: 15,
+  },
+  date: {
+    backgroundColor: '#EE7729',
+    padding: 15,
+    borderRadius: 15,
+    marginBottom: 20
+  },
   btnAlt:{
     width: 80,
     height: 80,
@@ -147,15 +231,8 @@ const styles = StyleSheet.create({
     width: 350,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    paddingBottom: 20
-  },
-  newBtn: {
-    width: 80,
-    height: 80,
-    backgroundColor: '#D67A46',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 10,
+    paddingTop: 40,
+    paddingBottom: 70
   },
   btnText:{
     color: 'white'
