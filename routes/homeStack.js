1a1eba4fa095b051a59bb6039834ec36050c3131
Vanshree <vanshreemathur@gmail.com>
2021-04-11 02:05:08 -0400

    Commented out homeStack.js (no need anymore)

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,27 +1,27 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
-import Home from '../screens/Home.js';
-import UserPost from '../screens/UserPost.js';
-import Stats from '../screens/Stats.js';
-import SignIn from '../screens/SignIn.js';
-
-const screens = {
-
-    // top screen is default
-    Home: {
-        screen: Home
-    },
-    Harvest: {
-        screen: UserPost
-    },
-    Stats: {
-        screen: Stats
-    },
-    SignIn: {
-      screen: SignIn
-    },
-}
-
-const HomeStack = createStackNavigator(screens);
-
-export default createAppContainer(HomeStack);
+// import { createStackNavigator } from 'react-navigation-stack';
+// import { createAppContainer } from 'react-navigation';
+// import Home from '../screens/Home.js';
+// import UserPost from '../screens/UserPost.js';
+// import Stats from '../screens/Stats.js';
+// import SignIn from '../screens/SignIn.js';
+//
+// const screens = {
+//
+//     // top screen is default
+//     Home: {
+//         screen: Home
+//     },
+//     Harvest: {
+//         screen: UserPost
+//     },
+//     Stats: {
+//         screen: Stats
+//     },
+//     SignIn: {
+//       screen: SignIn
+//     },
+// }
+//
+// const HomeStack = createStackNavigator(screens);
+//
+// export default createAppContainer(HomeStack);
