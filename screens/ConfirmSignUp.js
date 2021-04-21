647a5919c7f20b34dd552b8427e3207cf1e36747
Vanshree <vanshreemathur@gmail.com>
2021-04-21 04:33:54 -0400

    Added comments for Vanshree Pages

--- screens/ConfirmSignUp.js
+++ screens/ConfirmSignUp.js
@@ -1,22 +1,35 @@
+// Imported Libraries
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, Alert } from 'react-native';
 import { Auth } from 'aws-amplify';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
+
+//Export Function
 export default function ConfirmSignUp({ navigation }) {
+
+  //Initialize username and authentication code
   const [username, setUsername] = useState('');
   const [authCode, setAuthCode] = useState('');
+
+  //Confirm Sign-Up Function
   async function confirmSignUp() {
+
+    //Confirm that the authCode matches the code sent to username
     try {
       await Auth.confirmSignUp(username, authCode);
+      //Success Message in Console
       console.log('✅ Code confirmed');
+      //Navigate back to Sign-In page
       navigation.navigate('SignIn');
     } catch (error) {
+      //Else, print error message in console
       console.log(
         '❌ Verification code does not match. Please enter a valid verification code.',
         error.code
       );
+      //Show user error message -> ask them to retry
       Alert.alert(
         "Error",
         "❌ Verification code does not match. Please enter a valid verification code.",
@@ -29,10 +42,14 @@ export default function ConfirmSignUp({ navigation }) {
     }
   }
   return (
+    //Main Container
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
+        //Title
         <Text style={styles.title}>Confirm Sign Up</Text>
+        //Sub-title
         <Text style={styles.subtitle}> Check email for verification code </Text>
+        //Prompt user for username
         <AppTextInput
           value={username}
           onChangeText={text => setUsername(text)}
@@ -42,6 +59,7 @@ export default function ConfirmSignUp({ navigation }) {
           keyboardType="email-address"
           textContentType="emailAddress"
         />
+        //Prompt user for verification code
         <AppTextInput
           value={authCode}
           onChangeText={text => setAuthCode(text)}
@@ -49,28 +67,34 @@ export default function ConfirmSignUp({ navigation }) {
           placeholder="Enter verification code"
           keyboardType="numeric"
         />
+        //Confirm SignUp Button -> Will activate confirmSignUp function
         <AppButton title="Confirm Sign Up" onPress={confirmSignUp} />
       </View>
     </SafeAreaView>
   );
 }
 
+// Styling for Page
 const styles = StyleSheet.create({
+  // Safe-Area Container
   safeAreaContainer: {
     flex: 1,
     backgroundColor: '#29C16C'
   },
+  //Container
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  //Title
   title: {
     fontSize: 20,
     color: 'white',
     fontWeight: '500',
     marginVertical: 15
   },
+  //Sub-title
   subtitle: {
     fontSize: 20,
     color: 'white',
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,3 +1,4 @@
+// Imported Libraries
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, ImageBackground, Alert } from 'react-native';
 import { Auth } from 'aws-amplify';
@@ -5,18 +6,27 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import AppTextInput from '../components/AppTextInput';
 import AppButton from '../components/AppButton';
 
-// const backgroundImage = "../assets/mainImage.jpg"
-
+// Export Function
 export default function SignIn({ navigation, updateAuthState }) {
+
+  //Initialize username and password
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  //Sign-In Function
   async function signIn() {
+    //Confirm that the user exists
     try {
+      //Match username with password
       await Auth.signIn(username, password);
+      //Console -> Success Message
       console.log('✅ Success');
+      //Update Auth State -> User is logged in
       updateAuthState('loggedIn');
     } catch (error) {
+      //Else, console logs error
       console.log('❌ Error signing in...', error);
+      //Show user error message -> ask them to retry
       Alert.alert(
         "Error",
         "❌ Error signing in...Incorrect Credentials",
@@ -28,11 +38,16 @@ export default function SignIn({ navigation, updateAuthState }) {
       )
     }
   }
+
   return (
+    //Main Container
     <SafeAreaView style={styles.safeAreaContainer}>
       <View style={styles.container}>
+        //Title
         <Text style={styles.title}>Welcome to Nourish! </Text>
+        //Sub-title
         <Text style={styles.subtitle}> Harvest Data Collection App </Text>
+        //Prompt user for username
         <AppTextInput
           value={username}
           onChangeText={text => setUsername(text)}
@@ -42,6 +57,7 @@ export default function SignIn({ navigation, updateAuthState }) {
           keyboardType="email-address"
           textContentType="emailAddress"
         />
+        //Prompt user for password
         <AppTextInput
           value={password}
           onChangeText={text => setPassword(text)}
@@ -52,10 +68,14 @@ export default function SignIn({ navigation, updateAuthState }) {
           secureTextEntry
           textContentType="password"
         />
+        //Login Button -> Will activate the Sign-In function
         <AppButton title="Login" onPress={signIn} />
+        //Sign-Up Button for New Users
         <View style={styles.footerButtonContainer}>
+          //Navigate to Sign-Up Page
           <TouchableOpacity onPress={() => navigation.navigate('SignUp')}>
-            <Text style={styles.forgotPasswordButtonText}>
+            // Text within Sign-Up Button
+            <Text style={styles.signUpText}>
               Don't have an account? Sign Up
             </Text>
           </TouchableOpacity>
@@ -65,22 +85,27 @@ export default function SignIn({ navigation, updateAuthState }) {
   );
 }
 
+//Styling for Page
 const styles = StyleSheet.create({
+  //Safe-Area Container
   safeAreaContainer: {
     flex: 1,
     backgroundColor: '#29C16C'
   },
+  //Container
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
+  //Title
   title: {
     fontSize: 35,
     color: 'white',
     fontWeight: '500',
     marginVertical: 15
   },
+  //SUb-title
   subtitle: {
     fontSize: 20,
     color: 'white',
@@ -88,21 +113,16 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     fontStyle: 'italic'
   },
+  //Footer Container
   footerButtonContainer: {
     marginVertical: 15,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  forgotPasswordButtonText: {
+  //Sign-Up Button Text
+  signUpText: {
     color: 'white',
     fontSize: 18,
     fontWeight: '600'
   },
-  // backgroundContainer: {
-  //   flex: 1,
-  //   resizeMode: 'cover',
-  //   width: undefined,
-  //   height: undefined,
-  //   backgroundColor: '#889DAD',
-  // },
 });
